7ccfedfadab0f06eb96e1d8cfebf0cf526d3f90a
Halim <Halim@workstation>
2017-01-02 00:00:28 -0500

    welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,8 +13,6 @@
         <style>
             html, body {
                 background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
                 font-weight: 100;
                 height: 100vh;
                 margin: 0;
@@ -45,21 +43,10 @@
             }
 
             .title {
-                font-size: 84px;
+                font-size: 80px;
             }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
+            ul li{
+                margin-bottom: 10px
             }
         </style>
     </head>
@@ -76,6 +63,13 @@
                 <div class="title m-b-md">
                     Laravel REST API
                 </div>
+                <ul>
+                    <li><a href="{{ url('/api/users/') }}">Users</a></li>
+                    <li><a href="{{ url('/api/books/') }}">Books</a></li>
+                    <li><a href="{{ url('/api/authors/') }}">Authors</a></li>
+                    <li><a href="{{ url('/api/fields/') }}">Fields</a></li>
+                    <li><a href="{{ url('/api/languages/') }}">Languages</a></li>
+                </ul>
             </div>
         </div>
     </body>
